8df3ea32dee928e925c4afd5ff383a8bb703670a
Maximilian Hoppe <maxileonhoppe@googlemail.com>
2026-03-04 19:31:39 +0100

    Klasse Schüler hinzugefügt.

---
+++ Models/Schueler.cs
@@ -0,0 +1,25 @@
+﻿namespace Schuelerverwaltung.Models
+{
+    /// <summary>
+    /// Diese Klasse repräsentiert einen Schüler in der Schülerverwaltung. 
+    /// Sie enthält Informationen wie Name, Alter, Klasse und andere relevante Daten, die für die Verwaltung von Schülern erforderlich sind.
+    /// </summary>
+    public class Schueler
+    {
+        /// <summary>
+        /// Eindeutige Identifikationsnummer für den Schüler. 
+        /// Diese ID wird automatisch generiert, um sicherzustellen, dass jeder Schüler eine einzigartige Kennung hat.
+        /// </summary>
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        /// <summary>
+        /// Vorname des Schülers.
+        /// </summary>
+        public string Vorname { get; set; }
+
+        /// <summary>
+        /// Nachname des Schülers.
+        /// </summary>
+        public string Nachname { get; set; }
+    }
+}
