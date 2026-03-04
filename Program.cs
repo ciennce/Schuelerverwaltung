f1582a01ef8f29e9cd5b09094c465196acc96020
Maximilian Hoppe <maxileonhoppe@googlemail.com>
2026-03-04 20:26:53 +0100

    Implemented the GUI.

--- Program.cs
+++ Program.cs
@@ -5,29 +5,70 @@ using System.Drawing;
 
 namespace Schuelerverwaltung
 {
-    internal class Program
+    using Schuelerverwaltung.Logic;
+    using System;
+
+    class Program
     {
+        /// <summary>
+        /// Manager-Instanz, die die gesamte Logik für die Schülerverwaltung enthält.
+        /// </summary>
+        static SchuelerManager manager = new SchuelerManager();
+
         static void Main(string[] args)
         {
+            bool programmLaeuft = true;
 
-            var maxMustermann = new Schueler
+            while (programmLaeuft)
             {
-                Vorname = "Max",
-                Nachname = "Mustermann",
-            };
+                ZeigeHauptmenue();
 
-            var headers = new ColumnHeader[]
-            {
-                new ColumnHeader("Vorname"),
-                new ColumnHeader("Nachname"),
-            };
+                /// Liest die Benutzereingabe für die Menüauswahl. Die Eingabe wird nicht in der Konsole angezeigt (true-Parameter).
+                ConsoleKeyInfo key = Console.ReadKey(true);
 
+                switch (key.KeyChar)
+                {
+                    case '1':
+                        KonsoleVorbereiten("SCHÜLER EINSEHEN");
+                        /// Schüler einsehen
+                        SchuelerEinsehen();
+                        break;
+                    case 'q': /// Beenden (Das Programm)
+                        programmLaeuft = false;
+                        break;
+                }
+            }
+        }
+
+        static void ZeigeHauptmenue()
+        {
+            Console.Clear();
+            Console.WriteLine("=== SCHÜLERVERWALTUNG v1.0 ===");
+            var headers = new ColumnHeader[] {
+                new ColumnHeader("Einsehen [1]"),
+                new ColumnHeader("Hinzufügen [2]"),
+                new ColumnHeader("Bearbeiten [3]"),
+                new ColumnHeader("Löschen [4]"),
+                new ColumnHeader("Beenden [q]")
+            };
             var table = new Table(headers);
-            // table.Config = TableConfiguration
 
-            table.AddRow(maxMustermann.Nachname, maxMustermann.Vorname);
 
             Console.WriteLine(table.ToString());
         }
+
+        static void KonsoleVorbereiten(string titel)
+        {
+            Console.Clear();
+            Console.WriteLine($"--- {titel} ---");
+        }
+
+        static void SchuelerEinsehen()
+        {
+            Console.WriteLine("Schüler einsehen");
+            Console.ReadKey(true);
+        }
+
+        //static void
     }
 }
