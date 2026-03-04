b234c4dc336f178f19806e444148d2a1b51c9e12
Maximilian Hoppe <maxileonhoppe@googlemail.com>
2026-03-04 20:05:59 +0100

    Display student data in table format using BetterConsoleTables
    Replaced "Hello, World!" output with logic to create and display a table of student information. Added a sample Schueler object, defined table headers, and used BetterConsoleTables to format and print the data. Also imported necessary namespaces for table and model support.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Schuelerverwaltung
+﻿using BetterConsoleTables;
+using Schuelerverwaltung.Models;
+using System.Data.Common;
+using System.Drawing;
+
+namespace Schuelerverwaltung
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var maxMustermann = new Schueler
+            {
+                Vorname = "Max",
+                Nachname = "Mustermann",
+            };
+
+            var headers = new ColumnHeader[]
+            {
+                new ColumnHeader("Vorname"),
+                new ColumnHeader("Nachname"),
+            };
+
+            var table = new Table(headers);
+            // table.Config = TableConfiguration
+
+            table.AddRow(maxMustermann.Nachname, maxMustermann.Vorname);
+
+            Console.WriteLine(table.ToString());
         }
     }
 }
