4bca9509e6680cee1e8a4279835059ba17028c62
Maximilian Hoppe <maxileonhoppe@googlemail.com>
2026-03-05 19:33:33 +0100

    Code Clean Up

--- Models/Schueler.cs
+++ Models/Schueler.cs
@@ -27,38 +27,53 @@
         /// </summary>
         public string Klasse { get; set; } = string.Empty;
 
+        /// <summary>
         /// Notenschnitt des Schülers.
-
+        /// </summary>
         public int Notenschnitt { get; set; }
 
-        public string Faecher { get; set; } = string.Empty;
-
-
-        // Fächer
+        #region Fächer
 
         public string Mathematik { get; set; } = string.Empty;
+
         public string Deutsch { get; set; } = string.Empty;
+
         public string Englisch { get; set; } = string.Empty;
+
         public string Biologie { get; set; } = string.Empty;
+
         public string Geschichte { get; set; } = string.Empty;
+
         public string Kunst { get; set; } = string.Empty;
+
         public string Informatik { get; set; } = string.Empty;
+
         public string Sport { get; set; } = string.Empty;
+
         public string Musik { get; set; } = string.Empty;
+
         public string Physik { get; set; } = string.Empty;
+
         public string Chemie { get; set; } = string.Empty;
+
         public string Sozialwissenschaften { get; set; } = string.Empty;
+
         public string Französisch { get; set; } = string.Empty;
+
         public string Latein { get; set; } = string.Empty;
+
         public string Spanisch { get; set; } = string.Empty;
+
         public string Philosophie { get; set; } = string.Empty;
+
         public string KatholischeReligion { get; set; } = string.Empty;
+
         public string EvangelischeReligion { get; set; } = string.Empty;
+
         public string IslamischeReligion { get; set; } = string.Empty;
+
         public string Literatur { get; set; } = string.Empty;
 
-
-
-
+        #endregion
     }
 }
